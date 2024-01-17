d5afcd176c6cc237d23a6f8f36532cb4f389846c
Holden <holdenevers@gmail.com>
2024-01-17 15:18:24 -0800

    basic connection to backend application

--- react-assignment1/src/MyApp.js
+++ react-assignment1/src/MyApp.js
@@ -1,9 +1,10 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from './Table';
 import Form from './Form';
+import axios from 'axios';
 
 function MyApp() {
-   const [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -13,16 +14,35 @@ function MyApp() {
 
   }
 
+  async function fetchAll() {
+    try {
+       const response = await axios.get('http://localhost:8000/users');
+       return response.data.users_list;     
+    }
+
+    catch (error){
+       console.log(error); 
+       return false;         
+    }
+ }
+
+  useEffect(() => {
+    fetchAll().then( result => {
+      if (result)
+          setCharacters(result);
+    });
+  }, [] );
+
   function updateList(person) {
     setCharacters([...characters, person]);
   }
 
   return (
-      <div className="container">
-        <Table characterData={characters} removeCharacter={removeOneCharacter} />
-        <Form handleSubmit={updateList} />
-      </div>
-    );
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
+    </div>
+  );
 }
 
 export default MyApp;
